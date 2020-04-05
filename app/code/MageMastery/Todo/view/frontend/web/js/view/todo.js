a01714d7e2f2eb45e506aac44faa31edc6b37397
Cindy Wang <ciwang@trailappliances.com>
2020-04-05 16:01:16 -0700

    Lesson 15

--- app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -2,8 +2,8 @@ define(['uiComponent'], function (Component) {
     'use strict';
     console.log("component");
     return Component.extend({
-        // defaults: {
-        //     template: 'MageMastery_Todo/todo'
-        // }
+        defaults: {
+            template: 'MageMastery_Todo/todo'
+        }
     });
 });
