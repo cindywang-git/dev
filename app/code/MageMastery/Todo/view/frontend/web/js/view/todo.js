65b701da123bf46c10827ed72901e1653b838442
Cindy Wang <ciwang@trailappliances.com>
2020-04-07 17:04:47 -0700

    Lesson 16

--- app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,9 +1,14 @@
 define(['uiComponent'], function (Component) {
     'use strict';
-    console.log("component");
+    //console.log("component");
     return Component.extend({
         defaults: {
-            template: 'MageMastery_Todo/todo'
+            tasks: [
+                {label: "Task 1"},
+                {label: "Task 2"},
+                {label: "Task 3"},
+                {label: "Task 4"},
+            ]
         }
     });
 });
