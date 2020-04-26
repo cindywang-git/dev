01a3b23f87e58fcc82eb4351a8a058a669b00d42
Cindy Wang <ciwang@trailappliances.com>
2020-04-26 10:02:16 -0700

    Lesson 37

--- app/code/MageMastery/Todo/Model/ResourceModel/Task/Collection.php
+++ app/code/MageMastery/Todo/Model/ResourceModel/Task/Collection.php
@@ -2,15 +2,66 @@
 
 namespace MageMastery\Todo\Model\ResourceModel\Task;
 
+use MageMastery\Todo\Api\Data\TaskSearchResultInterface;
+use Magento\Framework\Api\Search\SearchCriteriaInterface;
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 use \MageMastery\Todo\Model\Task;
 use \MageMastery\Todo\Model\ResourceModel\Task as TaskResource;
 
-class Collection extends AbstractCollection
+class Collection extends AbstractCollection implements TaskSearchResultInterface
 {
 	protected function _construct()
 	{
 		$this->_init(Task::class, TaskResource::class);
 	}
-	
+
+    /**
+     * @return \Magento\Framework\Api\SearchCriteriaInterface|mixed
+     */
+	public function getSearchCriteria()
+    {
+        return $this->getSearchCriteria();
+    }
+
+    /**
+     * @param SearchCriteriaInterface $searchCriteria
+     * @return $this|TaskSearchResultInterface
+     */
+    public function setSearchCriteria(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria = null)
+    {
+        $this-$searchCriteria = $searchCriteria;
+        return $this;
+    }
+
+    /**
+     * @param array $items
+     * @return \Magento\Framework\Api\SearchResultsInterface|void
+     */
+    public function setItems(array $items = null)
+    {
+        if(!items) {
+            return $this;
+        }
+        foreach ($items as $item){
+            $this->addItem($item);
+        }
+        return $this;
+    }
+
+    /**
+     * @return int|void
+     */
+    public function getTotalCount()
+    {
+        $this->getSize();
+    }
+
+    /**
+     * @param int $totalCount
+     * @return $this|TaskSearchResultInterface
+     */
+    public function setTotalCount($totalCount)
+    {
+        return $this;
+    }
 }
