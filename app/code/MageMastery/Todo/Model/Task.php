5ddca38b78053d0925ee3d93f67b04eb293100ea
Cindy Wang <ciwang@trailappliances.com>
2020-05-20 15:38:40 -0700

    Lesson 50

--- app/code/MageMastery/Todo/Model/Task.php
+++ app/code/MageMastery/Todo/Model/Task.php
@@ -14,23 +14,62 @@ class Task extends AbstractModel implements TaskInterface
     const STATUS = 'status';
     const LABEL = 'label';
 
+    /**
+     *
+     */
     protected function _construct()
     {
         $this->_init(TaskResource::class);
     }
 
+    /**
+     * @return int
+     */
     public function getTaskId(): int
     {
         return (int) $this->getData(self::TASK_ID);
     }
 
+    /**
+     * @return string
+     */
     public function getStatus(): string
     {
         return $this->getData(self::STATUS);
     }
 
+    /**
+     * @return string
+     */
     public function getLabel(): string
     {
         return $this->getData(self::LABEL);
+    }
+
+    /**
+     * @param int $taskId
+     * @return mixed|void
+     */
+    public function setTaskId(int $taskId)
+    {
+        $this->setData(self::TASK_ID, $taskId);
+    }
+
+    /**
+     * @param string $status
+     * @return mixed|void
+     */
+    public function setStatus(string $status)
+    {
+        $this->setData(self::STATUS, $status);
+    }
+
+    /**
+     * @param string $label
+     * @return mixed|void
+     */
+    public function setLabel(string $label)
+    {
+        $this->setData(self::LABEL, $label);
     }
 }
