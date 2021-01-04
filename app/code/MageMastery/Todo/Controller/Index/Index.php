bf636d62e3f39b446a56433cf4321cc312ddf010
Cindy Wang <ciwang@trailappliances.com>
2021-01-03 20:00:11 -0800

    Lesson 62

--- app/code/MageMastery/Todo/Controller/Index/Index.php
+++ app/code/MageMastery/Todo/Controller/Index/Index.php
@@ -4,45 +4,35 @@ declare(strict_types=1);
 
 namespace MageMastery\Todo\Controller\Index;
 
-use MageMastery\Todo\Api\TaskManagementInterface;
-use MageMastery\Todo\Model\ResourceModel\Task as TaskResource;
-use MageMastery\Todo\Model\TaskFactory;
-use MageMastery\Todo\Service\TaskRepository;
-use Magento\Framework\Api\SearchCriteriaBuilder;
-use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\Redirect;
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
-    private $taskResource;
-
-    private $taskFactory;
-
-    private $taskRepository;
-
-    private $searchCriteriaBuilder;
-
-    private $taskManagement;
+    /**
+     * @var Session
+     */
+    private $session;
 
     public function __construct(
         Context $context,
-        TaskFactory $taskFactory,
-        TaskResource $task,
-        TaskRepository $repository,
-        SearchCriteriaBuilder $searchCriteriaBuilder,
-        TaskManagementInterface $taskManagement
-    ) {
-        $this->taskFactory = $taskFactory;
-        $this->taskResource = $task;
-        $this->taskRepository = $repository;
-        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-        $this->taskManagement = $taskManagement;
+        Session $session)
+    {
+        $this->session = $session;
         parent::__construct($context);
     }
 
     public function execute()
     {
+        if(!$this->session->isLoggedIn()){
+            /** @var Redirect $redirect */
+            $redirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+            $redirect->setPath('customer/account/login');
+            return $redirect;
+        }
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
