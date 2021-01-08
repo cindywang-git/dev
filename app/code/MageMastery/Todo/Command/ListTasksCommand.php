3bb3fbc9d679b4dfe6c5843568ea48e6f9840e4b
Cindy Wang <ciwang@trailappliances.com>
2021-01-08 11:27:03 -0800

    Bonus Lesson 67: Rendering tasks via CLI Command

--- app/code/MageMastery/Todo/Command/ListTasksCommand.php
+++ app/code/MageMastery/Todo/Command/ListTasksCommand.php
@@ -3,15 +3,35 @@ declare(strict_types=1);
 
 namespace MageMastery\Todo\Command;
 
+use MageMastery\Todo\Api\TaskRepositoryInterface;
+use Magento\Framework\Api\Search\SearchCriteriaBuilder;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Framework\Console\Cli;
 use Symfony\Component\Console\Input\InputArgument;
 
-class ListTasksCommand extends \Symfony\Component\Console\Command\Command
+class ListTasksCommand extends Command
 {
     const NAME = 'magemastery:todo:task-list';
+
+    /**
+     * @var TaskRepositoryInterface
+     */
+    private $taskRespository;
+
+    private $searchCriteriaBuilder;
+
+    public function __construct(
+        TaskRepositoryInterface $taskRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        String $name = null
+    )
+    {
+        $this->taskRespository = $taskRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        parent::__construct($name);
+    }
 
     protected function configure()
     {
@@ -24,7 +44,10 @@ class ListTasksCommand extends \Symfony\Component\Console\Command\Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Hello from CLI');
+        $taskSearchResult = $this->taskRespository->getList($this->searchCriteriaBuilder->create());
+        foreach($taskSearchResult->getItems() as $task){
+            $output->writeln($task->getTaskId().' '.$task->getLabel().'('.$task->getStatus().')');
+        }
         return Cli::RETURN_SUCCESS;
     }
 }
